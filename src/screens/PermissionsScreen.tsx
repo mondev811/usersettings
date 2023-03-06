90f4b4823dbab760773558afddba30782ae1cdec
Monica Rivera Pun <mondev811@gmail.com>
2023-03-05 23:26:35 -0800

    Add StyledButton

--- src/screens/PermissionsScreen.tsx
+++ src/screens/PermissionsScreen.tsx
@@ -1,17 +1,21 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React, {useContext} from 'react';
 
 import {PermissionsContext} from '../context/PermissionsContext';
+import StyledButton from '../components/StyledButton';
 
 export const PermissionsScreen = () => {
   const {permissions, askLocationPermission} = useContext(PermissionsContext);
 
   return (
     <View style={styles.container}>
-      <Text>PermissionsScreen</Text>
-
-      <Button title="Permission" onPress={askLocationPermission} />
-      <Text>{JSON.stringify(permissions, null, 4)}</Text>
+      <Text style={styles.mainText}>
+        This application needs permissions to use the device's GPS
+      </Text>
+      <StyledButton title="Set Permissions" onPress={askLocationPermission} />
+      <Text style={styles.secondaryText}>
+        {JSON.stringify(permissions, null, 4)}
+      </Text>
     </View>
   );
 };
@@ -21,5 +25,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  mainText: {
+    width: 250,
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  secondaryText: {
+    marginTop: 20,
   },
 });
